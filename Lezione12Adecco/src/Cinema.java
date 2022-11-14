f0ae9442b0ae95d6d8971bcdbe639f1512b00d72
hilemec <52612087+hilemec@users.noreply.github.com>
2022-11-14 12:04:51 +0100

    Add files via upload

---
+++ Lezione12Adecco/src/Cinema.java
@@ -0,0 +1,13 @@
+
+public class Cinema {
+	private Persona[] personeAlCinema;
+	
+	public Cinema() {
+		this.personeAlCinema = new Persona[5];
+	}
+	public void stampaCinema() {
+		for (Persona persona : this.personeAlCinema)
+			System.out.println(persona);			
+		}
+	public boolean 
+}
---
+++ Lezione12Adecco/src/Lez12es0.java
@@ -0,0 +1,10 @@
+/*
+ * principio di overloading
+ * si riferisce ai metodi di una classe ed è il sovraccarico in cui posso avere uno stesso nome di funzione ma
+ * queste verranno discriminate e differenziate attraverso ciò che si dice "Signature del Metodo" cioè un modo per
+ * identificare in modo univoco i metodi stessi della medesima classe tramite nome del metodo e i suoi parametri
+ *  in ingresso. Attenzione, nella signature non è compreso il ritorno del metodo stesso
+ */
+public class Lez12es0 {
+
+}
