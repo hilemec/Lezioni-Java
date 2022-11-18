25c62a6540f8517ccfb0bfe7b576c8b2bde1904b
hilemec <52612087+hilemec@users.noreply.github.com>
2022-11-18 12:58:36 +0100

    Add files via upload

---
+++ demo2/src/main/java/com/adecco/demo2/model/Fox.java
@@ -0,0 +1,14 @@
+package com.adecco.demo2.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Fox {
+	
+	private String image , link;
+
+}
